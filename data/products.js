8b53a8d3c803f666bc28373203aa0bd99091d598
somijan404 <gundelj.52@gmail.com>
2024-05-15 09:46:47 +0200

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -61,7 +61,31 @@ console.log(date);
 console.log(date.toLocaleTimeString());
 */
 
-export const products = [
+
+export let products = [];
+
+export function loadProducts(fun) {
+ const xhr =  new XMLHttpRequest();
+
+xhr.addEventListener('load', () => {
+ products = JSON.parse (xhr.response).map((productDetails) => {
+  if(productDetails.type === 'clothing') {
+    return new Clothing(productDetails);
+  }
+return new Product(productDetails);
+});
+
+console.log('load products');
+
+fun();
+});
+
+
+ xhr.open('GET', 'https://supersimplebackend.dev/products');
+ xhr.send();
+}
+
+/*export const products = [
 
   
   {
@@ -728,4 +752,4 @@ export const products = [
   }
 return new Product(productDetails);
 });
-
+*/
